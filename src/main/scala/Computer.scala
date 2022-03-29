a6896bbe287e4ff0b75965f00fea77761ebda087
m.sabatowski <ma.sabatowski@gmail.com>
2022-03-29 15:34:12 +0200

    First commit

--- src/main/scala/Computer.scala
+++ src/main/scala/Computer.scala
@@ -1,13 +1,20 @@
+import scala.io.Source
+
 object Computer extends App{
-  val board = new Board(4,4)
-//  val execute = new Executing(args(0))
-  board.createArrow(">",0,3)
-  board.printBoard()
-  board.move(0,1)
-  board.printBoard()
-  //board.createArrow("^",2,2)
-  board.move(0,40)
-//  board.move(1,2)
-//  board.rotate(1, 90)
-  board.printBoard()
+  println(args(0))
+  val lines = Source.fromFile(args(0)).getLines
+//  lines.foreach(println)
+  val board = new Board(10,10)
+  val execute = new Executing(args(0))
+  execute.execute
+
+//  board.createArrow(">",0,3)
+//  board.printBoard()
+//  board.move(0,1)
+//  board.printBoard()
+//  //board.createArrow("^",2,2)
+//  board.move(0,40)
+////  board.move(1,2)
+////  board.rotate(1, 90)
+//  board.printBoard()
 }
--- src/main/scala/Executing.scala
+++ src/main/scala/Executing.scala
@@ -5,27 +5,23 @@ class Executing(s: String) {
 
   val filename = s
 
-  def nth(ns: Iterator[String], n: Int): Iterator[String] = {
-    if (n == 1) ns.take(1)
-    else ns.drop(n - 1).take(1)
-  }
 
-  val num_lines = Source.fromFile(filename).getLines.size
+  def execute = {
+    val lines = Source.fromFile(s).getLines.toSeq
+    lines.foreach(println)
+    lines.map { line =>
+      println(line)
+      val com = line.split("\\s+")
+      com(0) match {
+        case "CREATE" => board.createArrow(com(1), com(2).toInt, com(3).toInt)
+        case "ROTATE" => board.rotate(com(1).toInt, com(2).toInt)
+        case "MOVE" => board.move(com(1).toInt, com(2).toInt)
+        case _ => println("Wrong command")
+      }
 
-  def line_to_list(s: String, n: Int): Seq[String] = {
-    nth(Source.fromFile(s).getLines, n).flatMap(line => line.split("\\s+")).toSeq
-  }
-
-  for {
-    i <- 1 to num_lines
-  } yield {
-    val com = line_to_list(filename, i)
-    if (com(0) == "CREATE") board.createArrow(com(1), com(2).toInt, com(3).toInt)
-    else if (com(0) == "ROTATE") board.rotate(com(1).toInt, com(2).toInt)
-    else if (com(0) == "MOVE") board.move(com(1).toInt, com(2).toInt)
-    else println("Wrong command")
-    board.printBoard()
-    println("_" * 10)
+      board.printBoard()
+      println("_" * 10)
+    }
   }
 
 }
